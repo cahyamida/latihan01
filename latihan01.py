a2a9ba4ce05db1b62fc304e53f45a21cf63ee0a3
cahyamida <cahyamidaf@gmail.com>
2020-10-12 13:06:37 +0700

    create new file

---
+++ latihan01.py
@@ -0,0 +1,14 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist)
+for x in mylist:
+	print(x)
+print(mylist[1])
+mylist = ["luke", "yoda", "qui-gon"]
+print(mylist)
+for x in mylist:
+	print(x)
+print(mylist[1])
+
