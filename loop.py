d8282cd2ae9118fb459af164399600c9d7b87ca7
cahyamida <cahyamidaf@gmail.com>
2020-10-17 21:27:54 +0700

    membetulkan eror di indentation break sama string before

--- loop.py
+++ loop.py
@@ -1,26 +1,24 @@
-#latihan 05
+# latihan 05
 
-#prints out 0,1,2,3,4
+# prints out 0,1,2,3,4
 count = 0
 while count < 5:
-	print (count)
-	count += 1  #This is the same as count = count+1
+    print(count)
+    count += 1  # This is the same as count = count+1
 
 # Prints out 0,1,2,3,4,5
 count = 0
-while True: 
-	print(count)
-	count += 1
-	if count >= 5:
-	break
-	
-#atau coba lagi
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+# atau coba lagi
 # Prints out 0,1,2,3,4,5
 count = 0
-while True: 
-	print(before:%d % count)
-	count += 1
-	if count >= 5:
-	break
-	
-	
+while True:
+    print('before: %d' % count )
+    count += 1
+    if count >= 5:
+        break
