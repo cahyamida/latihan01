4989654f62f0d09341954feb9df6dc29ef40c270
cahyamida <cahyamidaf@gmail.com>
2020-10-19 10:47:06 +0700

    exercise3 pertama

---
+++ exercise3.py
@@ -0,0 +1,16 @@
+# Exercise 3 : buat baris bilangan genap dan ganjil 1-10.
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# or
+for x in range(1, 11):
+    if x % 2 == 1:
+        print(x)
+
+#While Loops
+ls = [1, 5, 6, 10, 2]
+index = 0
+while index < len (ls):
+        print(ls)
--- latihan04.py
+++ latihan04.py
@@ -4,37 +4,37 @@ print(astring.index("o"))
 
 print(astring.count("l"))
 
-#[start:stop:step]
+# [start:stop:step]
 astring = "Hello world my name is Luke"
 print(astring[3:7:2])
 print(astring[::-1])
 print(astring.upper())
 print(astring.lower())
 
-#split
+# split
 astring = "Hello world!"
 afewwords = astring.split(",")
-print("Split sample %s " %astring.split(" "))
+print("Split sample %s " % astring.split(" "))
 
-#condition
+# condition
 x = 2
 if x == 2:
-	print(x)
-if "Hello" in alist:
+    print(x)
+if "Hello" in a list:
 
-	print("yes I have")		
-	print(len(alist))
-#if and else
-#if <condition> :
-	statement
-#elif <condition> :
-	statement
-#else :
-	statements
+    print("yes I have")
+    print(len(alist))
+# if and else
+# if <condition> :
+    statement
+# elif <condition> :
+    statement
+# else :
+    statements
 # The is operator
-x = [1,2,3]
-y = [1,2,3]
-print(x == y) # Prints out True
-print(x is y) # Prints out False
-print(not false) # Prints out True
-print((not false) == (False)) # Prints out False
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x == y)  # Prints out True
+print(x is y)  # Prints out False
+print(not false)  # Prints out True
+print((not false) == (False))  # Prints out False
